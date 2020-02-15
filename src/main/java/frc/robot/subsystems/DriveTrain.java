d48022d582da26525362629ce179ffa5567102e3
178Laptop <programming@farmingtonrobotics.org>
2020-02-15 17:26:36 -0500

    fixed merge conflict

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -78,12 +78,8 @@ public class DriveTrain extends Subsystem {
   }
 
   @Override
-<<<<<<< HEAD
   public void initDefaultCommand()
   {
     setDefaultCommand(new JoystickDrive());
-=======
-  protected void initDefaultCommand() {
->>>>>>> master
   }
 }
